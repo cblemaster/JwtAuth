c110935db8e013875701c419a10b31d6805233ce
Brian LeMaster <brian.lemaster@outlook.com>
2024-07-16 21:45:12 -0400

    added create role validator to core project

---
+++ JwtAuth.Core/Validation/CreateRoleDTOValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using JwtAuth.Core.DataTransferObjects;
+using Constants = JwtAuth.Core.Validation.ValidationConstants;
+using Rules = JwtAuth.Core.Validation.ValidationRules;
+using Errors = JwtAuth.Core.Validation.ValidationErrors;
+
+namespace JwtAuth.Core.Validation;
+
+public class CreateRoleDTOValidator : AbstractValidator<CreateRoleDTO>
+{
+    public CreateRoleDTOValidator()
+    {
+        RuleFor(r => r.Rolename)
+            .Must(r => Rules.StringIsRequiredRule(r))
+            .WithMessage(Errors.StringIsRequiredErrorMessage("Role Name"));
+        RuleFor(r => r.Rolename)
+            .Must(r => Rules.StringLengthMustBeLessThanOrEqualToMaxLengthRule(r, Constants.MAX_LENGTH_FOR_ROLE_ROLENAME))
+            .WithMessage(Errors.StringLengthMustBeLessThanOrEqualToMaxLengthErrorMessage("Role Name", Constants.MAX_LENGTH_FOR_ROLE_ROLENAME));
+        // TODO: Rule for unique rolename
+    }
+}

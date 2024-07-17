8cca8f64a315189473ab5dd760e48ce7fe40144c
Brian LeMaster <brian.lemaster@outlook.com>
2024-07-16 21:44:20 -0400

    added validation 'rules' to core project (I'm still not sure what these should be called)

---
+++ JwtAuth.Core/Validation/ValidationRules.cs
@@ -0,0 +1,16 @@
+﻿using JwtAuth.Core.DataTransferObjects;
+
+namespace JwtAuth.Core.Validation;
+
+public static class ValidationRules
+{
+    public static bool StringIsRequiredRule(string s) =>
+        !string.IsNullOrWhiteSpace(s) && s.Length > 0;
+    public static bool StringLengthMustBeLessThanOrEqualToMaxLengthRule(string s, int maxLength) =>
+        !string.IsNullOrWhiteSpace(s) && s.Length <= maxLength;
+    public static bool UserRolesMustNotBeEmptyRule(GetUserDTO user)
+        => user.Roles is not null && user.Roles.Any();
+    public static bool IdMustBeGreaterThanZeroRule(int id) => id > 0;
+    // value is unique
+    // will have to wait on data service
+}

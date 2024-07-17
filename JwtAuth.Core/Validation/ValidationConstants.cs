c69dacc310908649d216b46cbc12f40f600fefc9
Brian LeMaster <brian.lemaster@outlook.com>
2024-07-16 21:43:21 -0400

    added validation constants to core project

---
+++ JwtAuth.Core/Validation/ValidationConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace JwtAuth.Core.Validation;
+
+public static class ValidationConstants
+{
+    #region Data Schema Constants
+    public const int MAX_LENGTH_FOR_USER_USERNAME = 50;
+    public const int MAX_LENGTH_FOR_USER_PASSWORD = 50;
+
+    public const int MAX_LENGTH_FOR_PROFILE_FIRSTNAME = 255;
+    public const int MAX_LENGTH_FOR_PROFILE_LASTNAME = 255;
+    public const int MAX_LENGTH_FOR_PROFILE_EMAIL = 255;
+    public const int MAX_LENGTH_FOR_PROFILE_PHONE = 10;
+
+    public const int MAX_LENGTH_FOR_ROLE_ROLENAME = 50;
+    #endregion Data Schema Constants
+}

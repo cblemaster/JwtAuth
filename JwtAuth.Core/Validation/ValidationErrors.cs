70ff1c1cdf0c9bd7de27bc89f8865433641c279e
Brian LeMaster <brian.lemaster@outlook.com>
2024-07-16 21:43:37 -0400

    added validation errors to core project

---
+++ JwtAuth.Core/Validation/ValidationErrors.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtAuth.Core.Validation;
+
+public static class ValidationErrors
+{
+    public static string StringIsRequiredErrorMessage(string identifier) =>
+        $"{identifier} is required.";
+    public static string StringLengthMustBeLessThanOrEqualToMaxLengthErrorMessage(string identifier, int maxLength) =>
+        $"{identifier} must be {maxLength} characters or fewer.";
+    public static string UserRolesMustNotBeEmptyErrorMessage => "User must have one or more roles.";
+    public static string IdMustBeGreaterThanZeroErrorMessage(string identifier) => $"{identifier} must be greater than zero";
+}

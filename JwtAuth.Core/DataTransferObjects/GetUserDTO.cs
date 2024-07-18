8add06a3096fbb5624325279b2ca81847f47f902
Brian LeMaster <brian.lemaster@outlook.com>
2024-07-17 20:36:50 -0400

    updated dtos in core project to improve usability

--- JwtAuth.Core/DataTransferObjects/GetUserDTO.cs
+++ JwtAuth.Core/DataTransferObjects/GetUserDTO.cs
@@ -1,8 +1,7 @@
 ﻿namespace JwtAuth.Core.DataTransferObjects;
 
-public record GetUserDTO(int UserId, int ProfileId, string Username, string FirstName, string LastName,
-    string Email, string Phone, DateTime UserCreateDate, DateTime? UserUpdateDate, DateTime ProfileCreateDate,
-    DateTime? ProfileUpdateDate, IEnumerable<GetRoleDTO> Roles)
+public record GetUserDTO(int UserId, int ProfileId, string Username, DateTime CreateDate,
+    DateTime? UpdateDate, GetProfileDTO Profile, IEnumerable<GetRoleDTO> Roles)
 {
     public string? Token { get; set; }
 }

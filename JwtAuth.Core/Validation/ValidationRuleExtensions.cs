b63a55b5439908e6e44afae85610f2a9c9e5fe55
Brian LeMaster <brian.lemaster@outlook.com>
2024-07-19 14:25:06 -0400

    added validation extension methods in core project

---
+++ JwtAuth.Core/Validation/ValidationRuleExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace JwtAuth.Core.Validation;
+
+public static class ValidationRuleExtensions
+{
+    public static IRuleBuilderOptions<T, string> StringNotEmpty
+        <T>(this IRuleBuilder<T, string> ruleBuilder) =>
+        ruleBuilder.Must(s => !string.IsNullOrWhiteSpace(s))
+            .WithMessage("{PropertyName} is required.");
+
+    public static IRuleBuilderOptions<T, string> StringLengthDoesNotExceedMax
+        <T>(this IRuleBuilder<T, string> ruleBuilder, int max) =>
+            ruleBuilder.Must((rootObject, s, context) =>
+            {
+                context.MessageFormatter.AppendArgument("MaxLength", max);
+                return s.Length <= max;
+            })
+            .WithMessage("{PropertyName} must be {MaxLength} characters or fewer.");
+}

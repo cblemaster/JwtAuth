f9d949a17181b19eda3658b3119d4b38c894d31d
Brian LeMaster <brian.lemaster@outlook.com>
2024-07-20 19:33:19 -0400

    pseudo code for endpoints in program.cs in api project

--- JwtAuth.API/Program.cs
+++ JwtAuth.API/Program.cs
@@ -1,5 +1,6 @@
 using JwtAuth.API;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.Win32;
 
 WebApplicationBuilder webAppBuilder = WebApplication.CreateBuilder(args);
 IConfigurationRoot configRoot = webAppBuilder.CreateAndBuildConfigurationRoot();
@@ -10,4 +11,74 @@ webAppBuilder.RegisterAuthorization();
 webAppBuilder.RegisterDependencies(configRoot);
 WebApplication app = webAppBuilder.Build();
 app.MapGet("/", () => "Welcome to JwtAuth!");
+
+// register
+// /register
+// takes in: db context, password hasher, validation for (both) dtos, create user dto (which includes create profile dto)
+// validate both dtos
+// if not valid, return bad request with the validation errors
+// get password hash and salt
+// create new user entity
+// set entity props from dto
+// set password hash, salt on entity
+// get and set roles on the entity
+// set the profile on the entity
+// add the entity to the db, save changes
+// return the entity
+
+// login
+// /login
+// takes in: db context, password hasher, token generator, dto validation, login user dto
+// validate dto
+// if not valid, return bad request with the validation errors
+// find user entity by username
+// if null return unauthorized
+// verify hash match between entity hash and hashed dto password
+// if not match return unauthorized
+// return get user dto with token
+
+// add role
+// /role
+// takes in: db context, validation, create role dto
+// validate dto
+// if not valid, return bad request with the validation errors
+// create role entity and set props from dto
+// add entity to db
+// add the entity to the db, save changes
+// return the entity
+
+// change password
+// /user/{id}/password
+// takes in: db context, password hasher, validation, update user password dto
+// validate dto
+// if not valid, return bad request with the validation errors
+// get password hash and salt
+// get the user entity from db
+// if not found return not found
+// set paswword hash and salt on entity
+// save changes
+// return no content
+
+// change roles
+// /user/{id}/roles
+// takes in: db context, validation, update user roles dto
+// validate dto
+// if not valid, return bad request with the validation errors
+// get the user entity from db
+// if not found return not found
+// set roles on entity
+// save changes
+// return no content
+
+// update profile
+// /profile/{id}
+// takes in: db context, validation, update profile dto
+// validate dto
+// if not valid, return bad request with the validation errors
+// get the profile entity from db
+// if not found return not found
+// set entity props from dto
+// save changes
+// return no content
+
 app.Run();

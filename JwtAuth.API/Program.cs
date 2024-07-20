eca01f0edb7818ead4b59c8a600385ba78c72d0f
Brian LeMaster <brian.lemaster@outlook.com>
2024-07-20 18:26:11 -0400

    refactored program.cs in api project using extension methods

--- JwtAuth.API/Program.cs
+++ JwtAuth.API/Program.cs
@@ -1,61 +1,13 @@
-using JwtAuth.API.DataContext;
-using JwtAuth.Core.AuthenticationTools;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Text;
+using JwtAuth.API;
+using Microsoft.AspNetCore.Authentication;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-IConfigurationRoot config = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json")
-            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
-            .Build();
-
-string connectionString = config.GetConnectionString("Project") ?? "Error retrieving connection string!";
-string jwtSecret = config.GetValue<string>("JwtSecret") ?? "Error retreiving jwt config!";
-
-byte[] key = Encoding.ASCII.GetBytes(jwtSecret);
-JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[JwtRegisteredClaimNames.Sub] = "sub";
-
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(x =>
-{
-    x.RequireHttpsMetadata = false;
-    x.SaveToken = true;
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        NameClaimType = "name"
-    };
-});
-
-// TODO: Roles should be read from the db
-builder.Services.AddAuthorizationBuilder()
-    .AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"))
-    .AddPolicy("CustomerPolicy", policy => policy.RequireRole("Customer"))
-    .AddPolicy("VendorPolicy", policy => policy.RequireRole("Vendor"))
-    .AddPolicy("SalesPolicy", policy => policy.RequireRole("Sales"))
-    .AddPolicy("MarketingPolicy", policy => policy.RequireRole("Marketing"))
-    .AddPolicy("ReportingPolicy", policy => policy.RequireRole("Reporting"))
-    .AddPolicy("SupportPolicy", policy => policy.RequireRole("Support"));
-builder.Services.AddDbContext<JwtAuthContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services
-    .AddSingleton<ITokenGenerator>(tk => new JwtGenerator(jwtSecret))
-    .AddSingleton<IPasswordHasher>(ph => new PasswordHasher());
-
-WebApplication app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+WebApplicationBuilder webAppBuilder = WebApplication.CreateBuilder(args);
+IConfigurationRoot configRoot = webAppBuilder.CreateAndBuildConfigurationRoot();
+webAppBuilder.RegisterDbContext(configRoot);
+AuthenticationBuilder authBuilder = webAppBuilder.RegisterAuthentication();
+authBuilder.RegisterJwtBearer(configRoot);
+webAppBuilder.RegisterAuthorization();
+webAppBuilder.RegisterDependencies(configRoot);
+WebApplication app = webAppBuilder.Build();
+app.MapGet("/", () => "Welcome to JwtAuth!");
 app.Run();

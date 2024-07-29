8c68534b8f34f2f56439e0ac582d61f6c15b6f56
Brian LeMaster <brian.lemaster@outlook.com>
2024-07-29 19:33:25 -0400

    added scaffolded db context and entity classes to web project

---
+++ JwtAuth.Web/Entities/Role.cs
@@ -0,0 +1,14 @@
+﻿namespace JwtAuth.Web.Entities;
+
+public partial class Role
+{
+    public int RoleId { get; set; }
+
+    public string Rolename { get; set; } = null!;
+
+    public DateTime CreateDate { get; set; }
+
+    public DateTime? UpdateDate { get; set; }
+
+    public virtual ICollection<User> Users { get; set; } = [];
+}

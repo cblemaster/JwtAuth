c0bdc7f6ff14ccdeaef681ba32ca1e0bf2b9c84e
Brian LeMaster <brian.lemaster@outlook.com>
2024-08-02 18:09:45 -0400

    added community toolkit nuget packages to MAUI project

--- JwtAuth.MAUI/MauiProgram.cs
+++ JwtAuth.MAUI/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace JwtAuth.MAUI
 {
@@ -9,6 +10,7 @@ namespace JwtAuth.MAUI
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");

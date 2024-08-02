012cf076ed2d1cb01001699f571bcb09dce4022f
Brian LeMaster <brian.lemaster@outlook.com>
2024-08-02 18:47:24 -0400

    added current user class to MAUI project

---
+++ JwtAuth.MAUI/UserData/CurrentUser.cs
@@ -0,0 +1,13 @@
+﻿using JwtAuth.DataClient.DataTransferObjects;
+
+namespace JwtAuth.MAUI.UserData;
+
+internal static class CurrentUser
+{
+    private static GetUserDTO? user = null!;
+    public static void SetLogin(GetUserDTO? u) => user = u;
+    public static void SetLogout() => user = null!;
+    public static int GetUserId() => user is not null ? user.UserId : 0;
+    public static bool IsLoggedIn() => user is not null && !string.IsNullOrWhiteSpace(user.Token);
+    public static string GetToken() => user is not null ? user.Token : string.Empty;
+}

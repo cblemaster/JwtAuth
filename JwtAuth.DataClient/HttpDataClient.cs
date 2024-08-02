9bb47f70d684873892ac22bf134a639e8bfadf0a
Brian LeMaster <brian.lemaster@outlook.com>
2024-08-01 21:43:05 -0400

    added data client interface and implementation to data client project

---
+++ JwtAuth.DataClient/HttpDataClient.cs
@@ -0,0 +1,92 @@
+﻿using JwtAuth.DataClient.DataTransferObjects;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace JwtAuth.DataClient;
+
+public class HttpDataClient : IDataClient
+{
+    internal readonly HttpClient _client;
+    private const string BASE_URI = "https://localhost:7038/";
+
+    public HttpDataClient() => _client = new HttpClient
+    {
+        BaseAddress = new Uri(BASE_URI)
+    };
+
+    public async Task<GetUserDTO?> Register(RegisterUserDTO dto)
+    {
+        StringContent content = new(JsonSerializer.Serialize(dto));
+        content.Headers.ContentType = new("application/json");
+
+        try
+        {
+            HttpResponseMessage response = await _client.PostAsync("/register", content);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<GetUserDTO>();
+        }
+        catch (Exception) { throw; }
+    }
+    public async Task<GetUserDTO?> Login(LoginUserDTO dto)
+    {
+        StringContent content = new(JsonSerializer.Serialize(dto));
+        content.Headers.ContentType = new("application/json");
+
+        try
+        {
+            HttpResponseMessage response = await _client.PostAsync("/login", content);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<GetUserDTO>();
+        }
+        catch (Exception) { throw; }
+    }
+    public async Task<GetRoleDTO?> AddRole(AddRoleDTO dto)
+    {
+        StringContent content = new(JsonSerializer.Serialize(dto));
+        content.Headers.ContentType = new("application/json");
+
+        try
+        {
+            HttpResponseMessage response = await _client.PostAsync("/role", content);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<GetRoleDTO>();
+        }
+        catch (Exception) { throw; }
+    }
+    public async Task ChangeUserPassword(ChangeUserPasswordDTO dto, int id)
+    {
+        StringContent content = new(JsonSerializer.Serialize(dto));
+        content.Headers.ContentType = new("application/json");
+
+        try
+        {
+            HttpResponseMessage response = await _client.PutAsync($"/user/{id}/password", content);
+            response.EnsureSuccessStatusCode();
+        }
+        catch (Exception) { throw; }
+    }
+    public async Task ChangeUserRoles(ChangeUserRolesDTO dto, int id)
+    {
+        StringContent content = new(JsonSerializer.Serialize(dto));
+        content.Headers.ContentType = new("application/json");
+
+        try
+        {
+            HttpResponseMessage response = await _client.PutAsync($"/user/{id}/roles", content);
+            response.EnsureSuccessStatusCode();
+        }
+        catch (Exception) { throw; }
+    }
+    public async Task UpdateProfile(UpdateProfileDTO dto, int id)
+    {
+        StringContent content = new(JsonSerializer.Serialize(dto));
+        content.Headers.ContentType = new("application/json");
+
+        try
+        {
+            HttpResponseMessage response = await _client.PutAsync($"/profile/{id}", content);
+            response.EnsureSuccessStatusCode();
+        }
+        catch (Exception) { throw; }
+    }
+}
---
+++ JwtAuth.DataClient/IDataClient.cs
@@ -0,0 +1,13 @@
+﻿using JwtAuth.DataClient.DataTransferObjects;
+
+namespace JwtAuth.DataClient;
+
+public interface IDataClient
+{
+    Task<GetUserDTO?> Register(RegisterUserDTO dto);
+    Task<GetUserDTO?> Login(LoginUserDTO dto);
+    Task<GetRoleDTO?> AddRole(AddRoleDTO dto);
+    Task ChangeUserPassword(ChangeUserPasswordDTO dto, int id);
+    Task ChangeUserRoles(ChangeUserRolesDTO dto, int id);
+    Task UpdateProfile(UpdateProfileDTO dto, int id);
+}

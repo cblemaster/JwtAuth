bc8ed2b7872a82ee7c569d55842f965c599e16eb
Brian LeMaster <brian.lemaster@outlook.com>
2024-08-12 20:17:05 -0400

    refactored http data client in data client project

--- JwtAuth.DataClient/HttpDataClient.cs
+++ JwtAuth.DataClient/HttpDataClient.cs
@@ -9,74 +9,59 @@ public class HttpDataClient : IDataClient
     internal readonly HttpClient _client;
     private const string BASE_URI = "https://localhost:7038/";
 
-    public HttpDataClient() => _client = new HttpClient
+    public HttpDataClient() => _client =
+        new HttpClient { BaseAddress = new Uri(BASE_URI) };
+    
+    public async Task<GetUserDTO> RegisterAsync(RegisterUserDTO dto)
     {
-        BaseAddress = new Uri(BASE_URI)
-    };
-
-    public async Task<GetUserDTO?> RegisterAsync(RegisterUserDTO dto)
-    {
-        StringContent content = new(JsonSerializer.Serialize(dto));
-        content.Headers.ContentType = new("application/json");
+        StringContent content = GetHttpContentAsString<RegisterUserDTO>(dto);
 
         try
         {
-            HttpResponseMessage response = await _client.PostAsync("/register", content);
-            response.EnsureSuccessStatusCode();
-            return await response.Content.ReadFromJsonAsync<GetUserDTO>();
+            return await PostAsync<GetUserDTO>("/register", content);
         }
         catch (Exception) { throw; }
     }
-    public async Task<GetUserDTO?> LoginAsync(LoginUserDTO dto)
+    public async Task<GetUserDTO> LoginAsync(LoginUserDTO dto)
     {
-        StringContent content = new(JsonSerializer.Serialize(dto));
-        content.Headers.ContentType = new("application/json");
+        StringContent content = GetHttpContentAsString<LoginUserDTO>(dto);
 
         try
         {
-            HttpResponseMessage response = await _client.PostAsync("/login", content);
-            response.EnsureSuccessStatusCode();
-            return await response.Content.ReadFromJsonAsync<GetUserDTO>();
+            return await PostAsync<GetUserDTO>("/login", content);
         }
         catch (Exception) { throw; }
     }
     public async Task ChangeUserPasswordAsync(ChangeUserPasswordDTO dto, int id)
     {
-        StringContent content = new(JsonSerializer.Serialize(dto));
-        content.Headers.ContentType = new("application/json");
+        StringContent content = GetHttpContentAsString<ChangeUserPasswordDTO>(dto);
 
         try
         {
-            HttpResponseMessage response = await _client.PutAsync($"/user/{id}/password", content);
-            response.EnsureSuccessStatusCode();
+            await PutAsync($"/user/{id}/password", content);
         }
         catch (Exception) { throw; }
     }
     public async Task ChangeUserRolesAsync(ChangeUserRolesDTO dto, int id)
     {
-        StringContent content = new(JsonSerializer.Serialize(dto));
-        content.Headers.ContentType = new("application/json");
+        StringContent content = GetHttpContentAsString<ChangeUserRolesDTO>(dto);
 
         try
         {
-            HttpResponseMessage response = await _client.PutAsync($"/user/{id}/roles", content);
-            response.EnsureSuccessStatusCode();
+            await PutAsync($"/user/{id}/roles", content);
         }
         catch (Exception) { throw; }
     }
     public async Task UpdateUserProfileAsync(UpdateUserProfileDTO dto, int id)
     {
-        StringContent content = new(JsonSerializer.Serialize(dto));
-        content.Headers.ContentType = new("application/json");
+        StringContent content = GetHttpContentAsString<UpdateUserProfileDTO>(dto);
 
         try
         {
-            HttpResponseMessage response = await _client.PutAsync($"/user/{id}/profile", content);
-            response.EnsureSuccessStatusCode();
+            await PutAsync($"/user/{id}/profile", content);
         }
         catch (Exception) { throw; }
     }
-
     public async Task<IEnumerable<string>> GetRolesAsync()
     {
         try
@@ -84,6 +69,32 @@ public class HttpDataClient : IDataClient
             HttpResponseMessage response = await _client.GetAsync("/role");
             response.EnsureSuccessStatusCode();
             return response.Content.ReadFromJsonAsAsyncEnumerable<string>().ToBlockingEnumerable().Cast<string>();
+        }
+        catch (Exception) { throw; }
+    }
+    
+    private static StringContent GetHttpContentAsString<T>(T dto)
+    {
+        StringContent content = new(JsonSerializer.Serialize(dto));
+        content.Headers.ContentType = new("application/json");
+        return content;
+    }
+    private async Task<T> PostAsync<T>(string route, StringContent requestContent)
+    {
+        try
+        {
+            HttpResponseMessage response = await _client.PostAsync(route, requestContent);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<T>();
+        }
+        catch (Exception) { throw; }
+    }
+    private async Task PutAsync(string route, StringContent requestContent)
+    {
+        try
+        {
+            HttpResponseMessage response = await _client.PutAsync(route, requestContent);
+            response.EnsureSuccessStatusCode();
         }
         catch (Exception) { throw; }
     }

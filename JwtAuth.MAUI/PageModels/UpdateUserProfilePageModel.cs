70aae33df5afa3141dc78cfcab6e524c671c3c40
Brian LeMaster <brian.lemaster@outlook.com>
2024-08-19 21:23:12 -0400

    cleaned up update profile in MAUI project

--- JwtAuth.MAUI/PageModels/UpdateUserProfilePageModel.cs
+++ JwtAuth.MAUI/PageModels/UpdateUserProfilePageModel.cs
@@ -24,6 +24,8 @@ public partial class UpdateUserProfilePageModel : PageModelBase<UpdateUserProfil
         {
             await base._dataClient.UpdateUserProfileAsync(UpdateProfileUser, UpdateProfileUser.UserId);
             await base.CloseModalWindowAsync();
+            await Shell.Current.DisplayAlert("Success!", "Your profile has been updated, you have been logged out, and you will be directed to the login page.\nLog in again to see your profile changes.", "OK");
+            await Shell.Current.GoToAsync("///Login");
         }
         catch (Exception e) { await base.DisplayErrorAsync(e.Message); }
     }
--- JwtAuth.MAUI/PageModels/UserDetailPageModel.cs
+++ JwtAuth.MAUI/PageModels/UserDetailPageModel.cs
@@ -24,7 +24,17 @@ public partial class UserDetailPageModel : PageModelBase<GetUserDTO>
     [RelayCommand]
     private static async Task UpdateUserProfileAsync()
     {
-        await Shell.Current.Navigation.PushModalAsync(new UpdateUserProfilePage(new UpdateUserProfileDTO() { }));
+        await Shell.Current.Navigation.PushModalAsync
+            (new UpdateUserProfilePage
+                (new UpdateUserProfileDTO()
+                    {
+                        UserId = CurrentUser.UserId,
+                        Username = CurrentUser.Username,
+                        FirstName = CurrentUser.FirstName,
+                        LastName = CurrentUser.LastName,
+                        Email = CurrentUser.Email,
+                        Phone = CurrentUser.Phone
+                    }));
         return;
     }
 
